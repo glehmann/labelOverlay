810ea6ecea6600223f11747baba2f1450a7bbba7
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-19 20:46:09 +0100

    add wrappers and install devel files
    darcs-hash:20060119194609-2fc9d-09c831797850fae9a0610ad84f568ca76ddcaf29.gz

---
+++ Wrapping/wrap_itkLabelOverlayImageFilter.cmake
@@ -0,0 +1,10 @@
+WRAP_CLASS("LabelOverlayImageFilter" POINTER_WITH_SUPERCLASS)
+  FOREACH(d ${WRAP_DIMS})
+    COND_WRAP("${ITKM_IUS${d}}${ITKM_IUL${d}}${ITKM_IRGBUS${d}}" "${ITKT_IUS${d}},${ITKT_IUL${d}},${ITKT_IRGBUS${d}}" "US;RGBUS") # needed for watershed
+    COND_WRAP("${ITKM_IUS${d}}${ITKM_IUS${d}}${ITKM_IRGBUS${d}}" "${ITKT_IUS${d}},${ITKT_IUS${d}},${ITKT_IRGBUS${d}}" "US;RGBUS")
+    COND_WRAP("${ITKM_IUS${d}}${ITKM_IUC${d}}${ITKM_IRGBUS${d}}" "${ITKT_IUS${d}},${ITKT_IUC${d}},${ITKT_IRGBUS${d}}" "US;UC;RGBUS")
+    COND_WRAP("${ITKM_IUC${d}}${ITKM_IUL${d}}${ITKM_IRGBUC${d}}" "${ITKT_IUC${d}},${ITKT_IUL${d}},${ITKT_IRGBUC${d}}" "UC;RGBUC") # needed for watershed
+    COND_WRAP("${ITKM_IUC${d}}${ITKM_IUS${d}}${ITKM_IRGBUC${d}}" "${ITKT_IUC${d}},${ITKT_IUS${d}},${ITKT_IRGBUC${d}}" "UC;US;RGBUC")
+    COND_WRAP("${ITKM_IUC${d}}${ITKM_IUC${d}}${ITKM_IRGBUC${d}}" "${ITKT_IUC${d}},${ITKT_IUC${d}},${ITKT_IRGBUC${d}}" "UC;RGBUC")
+  ENDFOREACH(d)
+END_WRAP_CLASS()
