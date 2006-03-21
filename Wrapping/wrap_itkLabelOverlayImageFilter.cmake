59359287b47c5b6ad5ba3d606dcc8bc8cf20bbc7
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-21 20:45:10 +0100

    use WRAP_IMAGE_FILTER_COMBINATIONS
    darcs-hash:20060321194510-2fc9d-17981810b9780ad8b5a8aa89f53ff7cc697a09a2.gz

--- Wrapping/wrap_itkLabelOverlayImageFilter.cmake
+++ Wrapping/wrap_itkLabelOverlayImageFilter.cmake
@@ -1,14 +1,12 @@
 WRAP_CLASS("itk::LabelOverlayImageFilter" POINTER_WITH_SUPERCLASS)
   UNIQUE(label_types "${WRAP_ITK_INT};UL")
 
-  FOREACH(t ${label_types})
-    IF(WRAP_rgb_unsigned_short AND WRAP_unsigned_short)
-      WRAP_IMAGE_FILTER_TYPES(US ${t} RGBUS)
-    ENDIF(WRAP_rgb_unsigned_short AND WRAP_unsigned_short)
+  IF(WRAP_rgb_unsigned_short AND WRAP_unsigned_short)
+    WRAP_IMAGE_FILTER_COMBINATIONS(US "${label_types}" RGBUS)
+  ENDIF(WRAP_rgb_unsigned_short AND WRAP_unsigned_short)
 
-    IF(WRAP_rgb_unsigned_char AND WRAP_unsigned_char)
-      WRAP_IMAGE_FILTER_TYPES(UC ${t} RGBUC)
-    ENDIF(WRAP_rgb_unsigned_char AND WRAP_unsigned_char)
-  ENDFOREACH(t)
+  IF(WRAP_rgb_unsigned_char AND WRAP_unsigned_char)
+    WRAP_IMAGE_FILTER_COMBINATIONS(UC "${label_types}" RGBUC)
+  ENDIF(WRAP_rgb_unsigned_char AND WRAP_unsigned_char)
 
 END_WRAP_CLASS()
